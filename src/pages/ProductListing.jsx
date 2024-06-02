949ca7d49dd91bd18de50e5de8449f99ed5de415
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-02 22:03:43 +0530

    add price range

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -4,11 +4,24 @@ const ProductListing = () => {
     <>
       <Header />
       <div className="row">
-        <div className="col-md-4" style={{ background: "grey" }}>
-          Filters
-          <input type="range" min="100" max="200" value="150" />
+        <div className="col-md-4">
+          <div className="range-bar" style={{ width: "35%" }}>
+            Filters <br />
+            <div className="range-label d-flex justify-content-between">
+              <span>100</span>
+              <span>150</span>
+              <span>200</span>
+            </div>
+            <input
+              type="range"
+              min="100"
+              max="200"
+              value="150"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div className="col-md-8">bbwsfe</div>
         </div>
-        <div className="col-md-8">bbwsfe</div>
       </div>
     </>
   );
