cf97c2c4b056541880321c9cb1377d14e23848f7
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-02 21:56:12 +0530

    add routing using react router dom library.

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -3,6 +3,13 @@ const ProductListing = () => {
   return (
     <>
       <Header />
+      <div className="row">
+        <div className="col-md-4" style={{ background: "grey" }}>
+          Filters
+          <input type="range" min="100" max="200" value="150" />
+        </div>
+        <div className="col-md-8">bbwsfe</div>
+      </div>
     </>
   );
 };
