f7f00af0ab0d9ee270b290f6602623bb9db786f5
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-08 21:33:53 +0530

    add : footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="py-3 bg-body-secondary mt-5">
+      <footer className="text-center">
+        <p>&copy; POMA {new Date().getFullYear()} No rights reserved. </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
