18e95ad2b1f2555e5e25649313e08e25bfe021bc
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-11 16:04:19 +0530

    update : footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ const Footer = () => {
   return (
     <div className="py-3 bg-body-secondary mt-5">
       <footer className="text-center">
-        <p>&copy; POMA {new Date().getFullYear()} No rights reserved. </p>
+        <p>
+          &copy; POMA.shop {new Date().getFullYear()} No rights reserved.{" "}
+          <a href="https://github.com/checodezz/E-commerce-App" target="blank">
+            Github
+          </a>{" "}
+        </p>
       </footer>
     </div>
   );
